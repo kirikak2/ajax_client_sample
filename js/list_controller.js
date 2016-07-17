868f261f0d05ad2a66634b7cf8d3319408c28f5e
Toshio Maki <kirika.k2@gmail.com>
2016-07-17 13:04:57 +0900

    Support input_form

--- js/list_controller.js
+++ js/list_controller.js
@@ -2,10 +2,12 @@
 
 var app = angular.module("ajaxSample", [])
 app.controller("ListController", ['$scope', '$http', function($scope, $http){
+  $scope.searchParams = {};
   $scope.search = function(){
     $http({
       method: 'GET',
-      url: 'http://54.199.208.34:3000/addresses.json'
+      url: 'http://54.199.208.34:3000/addresses.json',
+      params: $scope.searchParams,
     }).success(function(data, status, headers, config) {
       $scope.results = data.results;
     })
